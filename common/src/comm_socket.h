0f24974864e8cf29387573ba0ec4b2ce751cacef
naoki009 <nyao@boo.nifty.jp>
2019-11-09 23:27:04 +0900

    Add license term

--- common/src/comm_socket.h
+++ common/src/comm_socket.h
@@ -1,3 +1,23 @@
+/*
+  Copyright 2019 coyote009
+
+  This file is part of coyote_racer.
+
+  coyote_racer is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 2 of the License, or
+  (at your option) any later version.
+
+  coyote_racer is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with coyote_racer.  If not, see <http://www.gnu.org/licenses/>.
+
+ */
+
 #ifndef COMM_SOCKET_H_
 #define COMM_SOCKET_H_
 
